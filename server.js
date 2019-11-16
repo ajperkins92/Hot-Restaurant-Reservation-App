308f031c683ee54b9a308c638d3356a39dc0f037
Alex Perkins <alex.jakob.perkins@gmail.com>
2019-11-16 11:06:58 -0800

    Adds npm dependencies for Express.js

--- server.js
+++ server.js
@@ -1,20 +1,21 @@
-var http = require("http");
+// Dependencies
+// =============================================================
+var express = require("express");
+var path = require("path");
 var fs = require("fs");
+
+// Sets up the Express App
+// =============================================================
+var app = express();
 var PORT = 3000;
 
-// Create a generic function to handle requests and responses
-function handleRequest(request, response) {
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
-  // Send the below string to the client when the user visits the PORT URL
-  response.end("It Works!! Path Hit: " + request.url);
-};
-// Use the Node HTTP package to create our server.
-// Pass the handleRequest function to empower it with functionality.
-var server = http.createServer(handleRequest);
 
-// Start our server so that it can begin listening to client requests.
-server.listen(PORT, function() {
-
-  // Log (server-side) when our server has started
-  console.log("Server listening on: http://localhost:" + PORT);
+// Starts the server to begin listening
+// =============================================================
+app.listen(PORT, function () {
+    console.log("App listening on PORT " + PORT);
 });
