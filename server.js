5a7a875dda97bf373d8cdba7550a287fe3a3fa06
Alex Perkins <alex.jakob.perkins@gmail.com>
2019-11-16 10:50:42 -0800

    Initial commit for Express server. Adds server.js

---
+++ server.js
@@ -0,0 +1,19 @@
+var http = require("http");
+var PORT = 3000;
+
+// Create a generic function to handle requests and responses
+function handleRequest(request, response) {
+
+  // Send the below string to the client when the user visits the PORT URL
+  response.end("It Works!! Path Hit: " + request.url);
+};
+// Use the Node HTTP package to create our server.
+// Pass the handleRequest function to empower it with functionality.
+var server = http.createServer(handleRequest);
+
+// Start our server so that it can begin listening to client requests.
+server.listen(PORT, function() {
+
+  // Log (server-side) when our server has started
+  console.log("Server listening on: http://localhost:" + PORT);
+});
